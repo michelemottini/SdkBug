d66532bde825c623096b4a7e6c611db721875c1a
Michele Mottini <mimo@careevolution.com>
2024-05-16 09:15:16 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+
+var test = new Test<Intermediate>();
+Console.WriteLine(test.X(intermediate => intermediate.Strings));
+
+class Intermediate
+{
+    public List<string> Strings { get; } = new List<string>();
+    public string String { get; set; } = string.Empty;
+}
+
+class Test<TIntermediate>
+{
+    public int X<TElement>(params Expression<Func<TIntermediate, TElement>>[] getItem)
+    {
+        return 0;
+    }
+
+    public int X<TElement>(params Expression<Func<TIntermediate, List<TElement>>>[] getList)
+    {
+        return 1;
+    }
+}
